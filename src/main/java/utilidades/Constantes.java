d5df9c6ba500b241f895c75e088b01b5796034db
S.Althaus <107248129+AlthausDev@users.noreply.github.com>
2023-10-18 03:27:35 +0200

    Delete src/main directory

--- src/main/java/utilidades/Constantes.java
+++
@@ -1,9 +0,0 @@
-package utilidades;
-
-public final class Constantes {
-	public final static String PATH_CREDENTIALS = "archivos/credenciales.txt";
-	public final static String PATH_STOPS = "archivos/paradas.dat";
-	public final static String PATH_USERS = "archivos/users.dat";
-	public final static String PATH_COUNTRIES = "src\\main\\resources\\paises.xml";
-}
-
