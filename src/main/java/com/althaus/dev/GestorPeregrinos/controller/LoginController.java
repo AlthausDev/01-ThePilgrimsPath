bbe74ca9258255b84931dd9e6bf17b6f5178d887
AlthausDev <samuelalthaus@gmail.com>
2023-12-31 02:04:52 +0100

    feat: Mejoradas medidas de validación de formato, agregado timeout al login en caso de repetidos intentos fallidos.

--- src/main/java/com/althaus/dev/GestorPeregrinos/controller/LoginController.java
+++ src/main/java/com/althaus/dev/GestorPeregrinos/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.althaus.dev.GestorPeregrinos.controller;
 import com.althaus.dev.GestorPeregrinos.app.AppLauncher;
 import com.althaus.dev.GestorPeregrinos.app.UserSession;
 import com.althaus.dev.GestorPeregrinos.service.CredencialesService;
+import com.althaus.dev.GestorPeregrinos.service.ValidationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,29 @@ import org.springframework.stereotype.Controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 @Controller
 public class LoginController {
 
+    private static final int MAX_INTENTOS_FALLIDOS = 5;
+    private static final long TIEMPO_BLOQUEO = 300000;
+    private int intentosFallidos = 1;
     private static final Logger logger = LoggerFactory.getLogger(AppLauncher.class);
     private final CredencialesService credencialesService;
+    private final ValidationService validationService;
     private UserSession userSession;
 
+
+
     @Autowired
-    public LoginController(CredencialesService credencialesService) {
+    public LoginController(CredencialesService credencialesService, ValidationService validationService) {
         this.credencialesService = credencialesService;
+        this.validationService = validationService;
 
         try {
             String path = "src/main/java/com/althaus/dev/GestorPeregrinos/view/login/Login.exe";
@@ -48,43 +60,43 @@ public class LoginController {
         }
     }
 
-    private void readStreamAsync(final java.io.InputStream stream) {
+    /**
+     * Lee de forma asíncrona las líneas de un InputStream y procesa las credenciales.
+     *
+     * @param stream InputStream para leer las líneas.
+     */
+
+    //Este método está pendiente de revisión. Llama a la validación de credenciales 2 veces por iteración.
+    private void readStreamAsync(final InputStream stream) {
         new Thread(() -> {
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuilder usernameBuilder = new StringBuilder();
-                StringBuilder passwordBuilder = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+                String username = null;
+                String password;
+
                 String line;
-
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    System.out.println("Linea: " + line);
 
-                    if (usernameBuilder.length() == 0) {
-                        usernameBuilder.append(line);
-                    } else if (passwordBuilder.length() == 0) {
-                        passwordBuilder.append(line);
-                    }
+                    if (username == null) {
+                        username = line;
+                    } else {
+                        password = line;
 
-                    if (usernameBuilder.length() > 0 && passwordBuilder.length() > 0) {
-                        String username = usernameBuilder.toString().toLowerCase();
-                        String password = passwordBuilder.toString();
-
-                        if (!username.isEmpty() && !password.isEmpty()) {
+                        if (validationService.validarDatosIngresados(username, password)) {
                             verificarCredenciales(username, password);
-                        } else {
-                            System.err.println("Los datos introducidos son incorrectos");
                         }
 
-                        usernameBuilder.setLength(0);
-                        passwordBuilder.setLength(0);
+                        // Reiniciar username y password para la siguiente iteración
+                        username = null;
+                        password = null;
                     }
                 }
-
-                } catch (IOException e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
     }
+
 
     private void verificarCredenciales(String username, String password) {
 
@@ -92,8 +104,49 @@ public class LoginController {
 
         if (inicioSesionExitoso) {
             System.out.println("Inicio de sesión exitoso.");
+            intentosFallidos = 0;
         } else {
-            System.out.println("Inicio de sesión fallido. Verifica tus credenciales.");
+            System.out.println("Inicio de sesión fallido. Verifica que las credenciales introducidas son correctas.");
+            System.out.println("Intentos fallidos: " + intentosFallidos++
+                    + "\nLimite de intentos: " + MAX_INTENTOS_FALLIDOS);
+             if (intentosFallidos > MAX_INTENTOS_FALLIDOS){
+                 bloquearPrograma();
+             }
         }
     }
+
+    private void bloquearPrograma() {
+        System.out.println("Se alcanzó el límite de intentos fallidos. El programa quedará bloqueado durante "
+                + TIEMPO_BLOQUEO / 1000 / 60 + " minutos.");
+
+        long tiempoInicio = System.currentTimeMillis();
+
+        try {
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    long tiempoTranscurrido = System.currentTimeMillis() - tiempoInicio;
+                    long tiempoRestante = TIEMPO_BLOQUEO - tiempoTranscurrido;
+
+                    if (tiempoRestante > 0) {
+                        long minutos = tiempoRestante / 1000 / 60;
+                        long segundos = (tiempoRestante / 1000) % 60;
+
+                        System.out.println("Tiempo restante: " + minutos + " minutos y " + segundos + " segundos");
+                    } else {
+                        System.out.println("Finalización del tiempo de espera. Intente iniciar sesión nuevamente.");
+                        intentosFallidos = 0;
+                        timer.cancel();
+                    }
+                }
+            }, 0, 5000);
+
+            Thread.sleep(TIEMPO_BLOQUEO);
+
+        } catch (InterruptedException e) {
+           System.out.println("Saliendo del programa");
+        }
+    }
+
 }
--- src/main/java/com/althaus/dev/GestorPeregrinos/service/ValidationService.java
+++ src/main/java/com/althaus/dev/GestorPeregrinos/service/ValidationService.java
@@ -3,30 +3,44 @@ package com.althaus.dev.GestorPeregrinos.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Scanner;
-
 @Service
 public class ValidationService {
-
-    private static final Scanner scanner = new Scanner(System.in);
 
     @Autowired
     private CredencialesService credencialesService;
 
-    public boolean validarNombreUsuarioExistente(String nombreUsuario) {
-        return credencialesService.existeUsuario(nombreUsuario);
+    public boolean validarDatosIngresados(String nombreUsuario, String password) {
+        return validarFormatoNombreUsuario(nombreUsuario) && validarFormatoPassword(password);
     }
+
     public boolean validarFormatoNombreUsuario(String nombreUsuario) {
         if (nombreUsuario.length() >= 3 && nombreUsuario.matches("[A-Za-z]+")) {
+            if (nombreUsuario.contains(" ")) {
+                System.err.println("El nombre de usuario no puede contener espacios en blanco. Vuelva a introducirlo.");
+                return false;
+            }
+            // Comentado por ahora
+            // if (credencialesService.existeUsuario(nombreUsuario)) {
+            //     System.err.println("El nombre de usuario ya existe. Introduce otro nombre de usuario.");
+            //     return false;
+            // }
             return true;
         } else {
-            System.err.println("El nombre debe tener al menos 3 caracteres y solo puede contener letras. Vuelva a introducirlo.");
+            System.err.println("El nombre de usuario debe tener al menos 3 caracteres y solo puede contener letras. Vuelva a introducirlo.");
             return false;
         }
     }
 
-    public boolean validarPassword(String password) {
-        return credencialesService.existePassword(password);
+    public boolean validarFormatoPassword(String password) {
+        if (password.length() >= 3) {
+            if (password.contains(" ")) {
+                System.err.println("La contraseña no puede contener espacios en blanco. Vuelva a introducirla.");
+                return false;
+            }
+            return true;
+        } else {
+            System.err.println("La contraseña debe tener al menos 3 caracteres. Vuelva a introducirla.");
+            return false;
+        }
     }
-
 }
